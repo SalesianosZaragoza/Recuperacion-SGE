1c4c71db46014bd0e0272574b4a8697bc227e16b
jefferson <shypocky@hotmail.com>
2020-01-23 23:11:44 +0100

    Se han arreglado errores que evitaban la instalación y se ha añadido la clase Author Signed-off-by: jefferson <shypocky@hotmail.com>

--- models.py
+++ models.py
@@ -3,7 +3,14 @@ from odoo import models, fields
 class Book(models.Model):
     _name = 'book.model'
 
-    name = fields.Char(string='Nombre', required=True)
-    description = fields.Text(string='Descripción', size=300, required=True)
-    release_date = fields.date(string="Fecha de salida", store=True, default=fields.date.today, required=True)
-    genres = fields.Selection(['Fantasia', 'Ciencia ficción', 'Romance', 'Aventura', 'Misterio', 'Distópica'], required=True, default='Fantasia')
+    name = fields.Char(string="Nombre")
+    description = fields.Text(string="Descripción")
+    release_date = fields.Date(string="Fecha de publicación", store=True)
+    genres = fields.Selection([('gen1', 'Fantasia'), ('gen2', 'Ciencia ficción'), ('gen3','Romance'), ('gen4','Aventura'), ('gen5','Misterio'), ('gen6', 'Distópica')], 'Género', default='gen1')
+
+class Author(models.Model):
+    _name = 'author.model'
+
+    name = fields.Char(string="Nombre")
+    surname = fields.Char(string="Apellido")
+    birth_date = fields.Date(string="Fecha de nacimiento", store=True)
