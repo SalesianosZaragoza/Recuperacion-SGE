d558f9400c5fb26064385bb1f621beaabfe6217f
jefferson <shypocky@hotmail.com>
2020-01-23 23:38:40 +0100

    Se ha añadido imports y nuevas funciones para el uso del Calendar Signed-off-by: jefferson <shypocky@hotmail.com>

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import timedelta
 
 class Book(models.Model):
     _name = 'book.model'
@@ -7,6 +8,26 @@ class Book(models.Model):
     description = fields.Text(string="Descripción")
     release_date = fields.Date(string="Fecha de publicación", store=True)
     genres = fields.Selection([('gen1', 'Fantasia'), ('gen2', 'Ciencia ficción'), ('gen3','Romance'), ('gen4','Aventura'), ('gen5','Misterio'), ('gen6', 'Distópica')], 'Género', default='gen1')
+    start_date = fields.Date(default=fields.Date.today)
+    duration = fields.Float(digits=(6, 2), help="Duration in days") 
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = r.start_date + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+            r.duration = (r.end_date - r.start_date).days + 1
 
 class Author(models.Model):
     _name = 'author.model'
@@ -14,3 +35,6 @@ class Author(models.Model):
     name = fields.Char(string="Nombre")
     surname = fields.Char(string="Apellido")
     birth_date = fields.Date(string="Fecha de nacimiento", store=True)
+
+
+    
