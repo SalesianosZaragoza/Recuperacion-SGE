49b29b41a5686bbb9266d42ef2b635f045681fac
jefferson <shypocky@hotmail.com>
2020-01-24 09:19:02 +0100

    se ha añadido model constraints y sql constraints Signed-off-by: jefferson <shypocky@hotmail.com>

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 from datetime import timedelta
 
 class Book(models.Model):
@@ -10,12 +10,22 @@ class Book(models.Model):
     genres = fields.Selection([('gen1', 'Fantasia'), ('gen2', 'Ciencia ficción'), ('gen3','Romance'), ('gen4','Aventura'), ('gen5','Misterio'), ('gen6', 'Distópica')], 'Género', default='gen1')
     start_date = fields.Date(string="Fecha de recordatorio", default=fields.Date.today)
 
+
 class Author(models.Model):
     _name = 'author.model'
 
     name = fields.Char(string="Nombre")
     surname = fields.Char(string="Apellido")
     birth_date = fields.Date(string="Fecha de nacimiento", store=True)
+    age = fields.Integer('age')
 
+    @api.constrains('age')
+    def _check(self):
+        if self.age < 18:
+            raise exceptions.ValidationError("La edad no puede ser inferior a 18")
 
-    
+    _sql_constraints = [
+        ('name_surname_check',
+        'CHECK(name != surname)',
+        "El nombre no puede ser el mismo que el del apellido")
+    ]
