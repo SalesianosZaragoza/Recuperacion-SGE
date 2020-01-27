e781ca6b10b00cdbaf956e266b3ae06cda871dae
Alberto <albertoluna92@gmail.com>
2020-01-27 18:43:04 +0100

    Creacion de los archivos principales Signed-off-by: Alberto <albertoluna92@gmail.com>

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "RecuperacionOdoo",
+
+    'summary': """ Lee el readme.md  """
+    'description': """
+       Module to recovery Odoo
+    """,
+
+
+    'author': "Alberto Luna Lopez",
+    'website': " ",
+
+    # Categories can be used to filter modules in modules listing
+    # for the full list
+    'category': 'test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'baseModule'],
+
+    # always loaded
+    'data': [
+        'libraryWriter.xml',
+        'security/ir.model.access.csv'
+    ],
+}
