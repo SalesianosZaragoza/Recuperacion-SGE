325dd15267a55d190efd6caa67f213db9a28badd
marcos <marcos.agni@gmail.com>
2021-01-23 18:35:51 +0100

    creacion de estructura de carpetas

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+
+{
+    'name': "recuperacionOdoo",
+
+    'summary': """modulo de recuperacion""",
+
+    'description': """
+        modulo recuperacion
+    """,
+
+    'author': "Marcos Garcés",
+    'website': "http://www.yourcompany.com",
+    'category': 'Test',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'view.xml',
+    ],
+
+}
