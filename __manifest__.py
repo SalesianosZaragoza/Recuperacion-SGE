c2a3aab08e8b241e7477c5bb21e6a984f8d38996
Jorch00 <jorgetardi@gmail.com>
2021-01-27 18:40:46 +0100

    1-Organizando el proyecto

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+
+   # -- coding: utf-8 --
+{
+    'name': "App Natural Parks",
+
+    'summary': """Una aplicación de parques naturales""",
+
+    'description': """
+        Una aplicación sobre los parques naturales de España
+                - visitantes 
+                - staff de los parques 
+                - acomodacion
+    """,
+
+    'author': "Jorge Gimeno",
+    'website': "",
+
+    # Categories can be used to filter modules in modules listing
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'board'],
+
+    # always loaded
+    'data': [
+       
+       
+        
+    ],
+} 
