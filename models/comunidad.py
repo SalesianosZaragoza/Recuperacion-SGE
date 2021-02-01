3364bdc1325841bcf89880903b54f32e4ced006e
Cristian Marin <marin.gicri18@zaragoza.salesianos.edu>
2021-02-01 22:33:01 +0100

    Creacion de carpetas/Comunidad

---
+++ models/comunidad.py
@@ -0,0 +1,8 @@
+from odoo import models, fields 
+
+class comunidad(models.Model):
+    _name = 'np.comunidad'
+
+    nombre = fields.Char(string="nombre", required=True)
+    extension = fields.Integer(string="extension", required=True)
+    responsable = fields.Char(required=True)
